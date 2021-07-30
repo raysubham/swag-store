fa6cd0da6eb2f925dcf1fa18d18e2437d558bd37
Subham Ray <self.programmed@gmail.com>
2021-07-30 16:37:02 +0530

    added loader

--- frontend/components/Loading.js
+++ frontend/components/Loading.js
@@ -1,5 +1,15 @@
+import Loader from 'react-loader-spinner'
+
 const Loading = () => {
-  return <p>Loading...</p>
+  return (
+    <Loader
+      type='TailSpin'
+      color='#ff0000'
+      height={100}
+      width={100}
+      timeout={1052415000}
+    />
+  )
 }
 
 export default Loading
